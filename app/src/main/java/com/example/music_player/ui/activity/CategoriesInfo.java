dfb15de15f44dcf1a1129217680a99589882d646
Sepide-naderi <sepidenaderi82@gmail.com>
2023-01-07 19:35:55 +0330

    third commit--> Add logo,Add some libraries,Add theme,Add CategoriesInfo

---
+++ app/src/main/java/com/example/music_player/ui/activity/CategoriesInfo.java
@@ -0,0 +1,23 @@
+package com.example.music_player.ui.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.music_player.R;
+import com.example.music_player.remote.APIService.music.MusicRepository;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CategoriesInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories_info);
+
+    }
+
+
+}
+
